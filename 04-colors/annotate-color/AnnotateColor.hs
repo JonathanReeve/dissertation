408afa5897b74b31135eb5aaccd80a4240b1236d
Jonathan Reeve <jon.reeve@gmail.com>
2020-02-11 12:18:16 -0500

    working color annotator now

--- 04-colors/annotate-color/AnnotateColor.hs
+++ 04-colors/annotate-color/AnnotateColor.hs
@@ -2,48 +2,62 @@
 
 module AnnotateColor where
 
+import qualified Clay as C
+import Codec.Text.Detect (detectEncoding)
+import Control.Applicative ((<|>), empty)
+import Control.Monad (forM_)
+import Data.Attoparsec.Text as AT
+import Data.Char
+import Data.Either
+import Data.Function (on)
 import Data.List (intersperse, sort, sortBy)
 import Lucid
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text.IO as TIO
-import Data.Function (on)
-import Replace.Attoparsec.Text
-import Data.Attoparsec.Text as AT
-import qualified Data.Text as T
-import Data.Either
-import Data.Char
-import qualified Data.Map.Strict as M
-import Control.Applicative ((<|>), empty)
 import Options.Generic
+import Replace.Attoparsec.Text
+import qualified Data.Map.Strict as M
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Encoding as TE
 
 -- | Just some useful aliases here
 type ColorWord = T.Text
 type Hex = T.Text
 
 wordBoundary :: Parser Char
-wordBoundary = satisfy (inClass " \n\r\"\'")
+wordBoundary = space <|> satisfy isPunctuation
 
 -- | Takes a list of words like "light green blue" and makes a
 -- parser which will find "light green blue" and also "light green-blue",
 -- "light green\nblue" and so on.
 wordListParser :: [T.Text] -> Parser T.Text
 wordListParser [w] = do -- One word case
-  boundaryBefore <- wordBoundary
   word <- asciiCI w
-  boundaryAfter <- wordBoundary
   return word
 wordListParser (w:ws) = do  -- Multi-word case
-  satisfy (inClass " \n\r\"\'")  -- Word boundary first
-  a <- asciiCI w                  -- word, case insensitive
-  b <- satisfy (inClass " -\n\r") -- a separator
-  c <- wordListParser ws          -- more words
-  return (a `T.append` (T.singleton b) `T.append` c) -- singleton :: Char -> Text
+  a <- asciiCI w    -- single word
+  b <- space <|> char '-' -- followed by a separator
+  c <- wordListParser ws          -- and more words
+  -- Parse to space-separated.
+  return (a `T.append` (T.singleton ' ') `T.append` c) -- singleton :: Char -> Text
+
+
+-- | Don't just parse the word, parse it with word boundaries
+-- on either side.
+withBoundaries :: Parser T.Text -> Parser T.Text
+withBoundaries word = do
+  wordBoundary
+  w <- word
+  wordBoundary
+  return w
 
 -- | Make one big parser out of our color map, and the expressions
 -- generated from wordListParser.
 colorParser :: [(ColorWord, Hex)] -> Parser T.Text
-colorParser colormap = choice $ map (wordListParser . T.words . fst) $ colormap
+colorParser colorMap = choice $ map withBoundaries parsers where
+  parsers = map (wordListParser . T.words . fst) $ colorMap
 
 -- | Makes HTML from a color word and hex pair.
 -- I.e. "red" -> "<span class="color" style="color: #ff0000">"
@@ -66,60 +80,56 @@ xkcdMap rawMap = reverse $ sortBy (compare `on` T.length . fst) unsorted
     textLines = tail $ T.lines rawMap
     unsorted = [ mapTuple T.strip ( T.breakOn "\t" ln ) | ln <- textLines ]
 
--- | The parser returns what it parsed, which may or may not contain
--- hyphens. But we want the non-hyphenated version so that we can
--- look up its hex in the color map. It sucks that we have to
--- look up the hex code again, but I can't think of a better way.
--- So this cleans up the color expression as found by the parser,
--- so that it can be used by the lookup.
-punctToSpace :: T.Text -> T.Text
-punctToSpace str = T.map p2s str where
-  p2s = (\c -> if T.isInfixOf (T.singleton c) "-\n\r" then ' ' else c)
+annotate :: M.Map ColorWord Hex -> [Either T.Text (T.Text, T.Text)] -> T.Text
+annotate colorMapMap results = T.concat $ map processBlock results where
+  processBlock :: Either T.Text (T.Text, T.Text) -> T.Text
+  processBlock block = case block of
+    Left txt -> txt
+    Right (textFormat, stdFormat) -> case (colorMapMap M.!? stdFormat) of
+      Nothing -> T.concat ["CANTFIND", stdFormat]
+      Just hex -> TL.toStrict $ makeSpan textFormat hex
 
--- | Using a map-ified version of our color map, this looks
--- up each word found by the parser, and if found, turns it
--- into HTML, highlighting it using its color.
-annotate :: M.Map ColorWord Hex -> T.Text -> T.Text
-annotate cmm color = case cmm M.!? ((punctToSpace . T.strip) color) of
-  Nothing -> (T.concat ["CANTFIND", color])
-  Just hex -> TL.toStrict $ makeSpan (T.strip color) hex
+findReplace :: Parser T.Text -> T.Text -> [Either T.Text (T.Text, T.Text)]
+findReplace parser sourceText = fromRight [] $ parseOnly (findAllCap parser) sourceText
 
--- | Adds the parser pattern to the word - hex tuple.
-makeColorParser :: (ColorWord, Hex) -> (ColorWord, Hex, Parser T.Text)
-makeColorParser (colorword, hex) = (colorword, hex, ((wordListParser . T.words) colorword))
+-- | Make a standard HTML page from the results,
+-- by scaffolding it with an HTML template.
+scaffold :: T.Text -> T.Text
+scaffold text = TL.toStrict $ renderText $
+  html_ [] $ do
+    head_ [] $ do
+      style_ [type_ "text/css"] $ C.render css
+    body_ [] $ toHtmlRaw text
 
-findReplace :: (ColorWord, Hex, Parser T.Text) -> T.Text -> [Either T.Text T.Text]
-findReplace (colorWord, hex, parser) sourceText = fromRight [] $ parseOnly (findAll parser) sourceText
-
--- | Only acts on the remaining unparsed text.
-replaceRemaining :: (ColorWord, Hex, Parser T.Text) -> [Either T.Text T.Text] -> [Either T.Text T.Text]
-replaceRemaining (colorWord, hex, parser) sourceText = concatMap parseBit sourceText where
-  parseBit :: Either T.Text T.Text -> [Either T.Text T.Text]
-  parseBit textBit = case textBit of
-    Left t -> findReplace (colorWord, hex, parser) t -- Unparsed, parse it
-    Right t -> [Right t] -- Parsed, pass through
-
+css :: C.Css
+css = "body" C.? do
+         C.backgroundColor "#333"
+         C.color "#ddd"
 
 main :: IO ()
 main = do
-    -- Load color map. (Data from https://xkcd.com/color/rgb.txt )
-    rawText <- TIO.readFile "../data/xkcd/rgb.txt"
-    -- Process color map
-    let colorMap = xkcdMap rawText
-    -- Make Data.Map map out of it
-    -- let cmm = M.fromList cm
+   -- Load color map. (Data from https://xkcd.com/color/rgb.txt )
+   rawText <- TIO.readFile "../data/xkcd/rgb.txt"
+   -- Process color map
+   let colorMap = xkcdMap rawText
+   -- Make Data.Map map out of it
+   let colorMapMap = M.fromList colorMap
 
-    -- Add parsers to the map
-    let colorMapWithParsers = map makeColorParser colorMap
+   -- Parse command-line argument, and read the filename given
+   -- by the first argument.
+   fileName <- getRecord "Color word annotator."
+   -- inFile <- TIO.readFile fileName
+   rawByteStr <- B.readFile fileName
 
-    -- Parse command-line argument, and read the filename given
-    -- by the first argument.
-    fileName <- getRecord "Color word annotator."
-    inFile <- TIO.readFile fileName
-    -- Run the parser, annotate it, print the results.
-    -- TIO.putStr $ streamEdit (colorParser cm) (annotate cmm) inFile
+   -- Try to decode Utf-8 first, but if not, try Latin1.
+   let inFile = case TE.decodeUtf8' rawByteStr of
+                  Left err -> TE.decodeLatin1 rawByteStr
+                  Right text -> text
 
-    let firstReplaced = findReplace (head colorMapWithParsers) inFile
-    let allReplaced = foldl (map (\mapping -> replaceRemaining mapping) colorMapWithParsers) firstReplaced
+   let firstReplaced = findReplace (colorParser colorMap) inFile
 
-    print allReplaced
+   -- print firstReplaced
+   let annotated = annotate colorMapMap firstReplaced
+   let scaffolded = scaffold annotated
+
+   TIO.putStr $ scaffolded
