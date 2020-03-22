4eddd553ab9b2d5e8c263ad62cf52c769f02495f
Jonathan Reeve <jon.reeve@gmail.com>
2020-03-22 14:43:41 -0400

    working web example now

--- 04-colors/annotate-color/Web.hs
+++ 04-colors/annotate-color/Web.hs
@@ -1,23 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import           Control.Monad                        (join)
 import           Control.Applicative                  ((<$>))
-import           Controllers.Home                     (home, login, post)
 import           Data.Maybe                           (fromMaybe)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import           Network.Wai.Middleware.Static        (addBase, noDots,
                                                        staticPolicy, (>->))
 import           System.Environment                   (lookupEnv)
 import           Text.Read                            (readMaybe)
-import           Web.Scotty                           (middleware, scotty)
+import           Web.Scotty                           (middleware, scotty, get, param, html)
 
 main :: IO ()
 main = do
   port <- fromMaybe 3000
-        . join
-        . fmap readMaybe <$> lookupEnv "PORT"
+    . join
+    . fmap readMaybe <$> lookupEnv "PORT"
   scotty port $ do
-         middleware $ staticPolicy (noDots >-> addBase "static/images") -- for favicon.ico
-         middleware logStdoutDev
-         home >> login >> post
+    -- middleware $ staticPolicy (noDots >-> addBase "static/images") -- for favicon.ico
+    middleware logStdoutDev
+    get "/:word" $ do
+      beam <- param "word"
+      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
