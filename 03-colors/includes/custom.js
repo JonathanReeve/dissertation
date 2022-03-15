189736990cddb222fef8b48f3dfe7ded2141bfbe
Jonathan Reeve <jonathan.reeve@columbia.edu>
2022-03-15 16:11:13 -0400

    JavaScript hack for citations inside blocks

--- 03-colors/includes/custom.js
+++ 03-colors/includes/custom.js
@@ -1,11 +1,17 @@
 $(function() {
+    // Engage lazy loading. But does this even work?
     $('.lazy').Lazy();
     // Captions should be margin notes by default.
     $('figcaption').addClass('marginnote');
 });
 
+// Initialize Mermaid, for charts.
 mermaid.initialize(
     { startOnLoad: true,
       securityLevel: 'loose' // needed for html inlines
     }
 );
+
+
+// Dirty hack to make citations appear inline with color-annotated blocks.
+$('div.annotated p > span.citation').parent().css('display', 'inline')
