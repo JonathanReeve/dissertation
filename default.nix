0a49acbb722b72c2e2b92b2d6c6964dbac3a7f2b
Jonathan Reeve <jon.reeve@gmail.com>
2022-07-31 16:06:52 -0400

    new nixpkgs reference

--- default.nix
+++ default.nix
@@ -1,4 +1,12 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{nixpkgs ? import (builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-2022-07-31";
+  # Commit hash for nixos-unstable as of 2018-09-12
+  url = "https://github.com/nixos/nixpkgs/archive/7b9be38c7250b22d829ab6effdee90d5e40c6e5c.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1jgcsgzvxnc27wby4n7jyp5w67461qm5x519xkpz2h14zbjjndm7";
+}) {}
+}:
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
