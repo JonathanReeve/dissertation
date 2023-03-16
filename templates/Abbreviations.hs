5cf8de707a80a60a5369a70ffea80722187eb533
Jonathan Reeve <jon.reeve@gmail.com>
2023-03-16 00:16:18 -0700

    just some more editing

--- templates/Abbreviations.hs
+++ templates/Abbreviations.hs
@@ -9,6 +9,7 @@ abbreviations = fromList [
     ("OED", "Oxford English Dictionary")
   , ("C_{PG}", "Corpus: Project Gutenberg subset")
   , ("C_{PG2}", "Corpus: Project Gutenberg subset, 1800—1922")
+  , ("C_{NG}", "Corpus: Google NGrams")
   , ("LCSH", "Library of Congress Subject Heading")
   , ("LCC", "Library of Congress Category")
   , ("TEI", "Text Encoding Initiative")
@@ -19,4 +20,6 @@ abbreviations = fromList [
   , ("RDF", "Resource Description Framework")
   , ("ALTO", "Analyzed Layout and Text Object")
   , ("CLI", "command line interface")
+  , ("NER", "named entity recognition")
+  , ("WSD", "word sense disambiguation")
   ]
