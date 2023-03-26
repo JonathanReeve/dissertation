f0616053a38ae985fe4e2faf0da5e30b61d72932
Jonathan Reeve <jon.reeve@gmail.com>
2023-03-26 15:39:31 -0700

    Partially fix fig captions in margins

--- assets/custom.js
+++ assets/custom.js
@@ -1,9 +1,3 @@
-// Dirty hack to make citations appear inline with color-annotated blocks.
-// Translate to JavaScript:
-// $('div.annotated p > span.citation').parent().css('display', 'inline')
-document.querySelectorAll('div.annotated p > span.citation').forEach(
-    (el) => (el.parentNode.style.display('inline'))
-)
 
 // Captions should be margin notes by default.
 document.querySelectorAll('figcaption').forEach((el) => el.classList.add('marginnote'))
@@ -18,4 +12,11 @@ mermaid.initialize(
     { startOnLoad: true,
       securityLevel: 'loose' // needed for html inlines
     }
+)
+
+// Dirty hack to make citations appear inline with color-annotated blocks.
+// Translated from this JQUery:
+// $('div.annotated p > span.citation').parent().css('display', 'inline')
+document.querySelectorAll('div.annotated p > span.citation').forEach(
+    (el) => (el.parentNode.style.display('inline'))
 )
